7e9663a2dad87d57fdc36cc3636aae2741f5c7d7
Caiden701 <moreno.caiden02@gmail.com>
2024-07-12 15:18:32 -0400

    Add files via upload

---
+++ Lab3/main.c
@@ -0,0 +1,149 @@
+/*
+ * Lab3_Stepper_Motor.c
+ *
+ * Created: 9/3/2023 4:50:36 PM
+ * Author: Caiden Moreno
+ 
+ Project name: Three_Modes_stepper
+ Description: Control stepper motor using three different modes: 
+ Wave step mode
+ Full step mode 
+ Half step mode
+ 
+ Three arrays were created to associate the bit patter necessary to move
+ the pattern required for each mode. The code also has at least two for loops 
+ for each mode to help drive the stepper functions. The program also utilizes
+ the delay function in between each step to see more clearly the motors movement 
+ and functionality.
+ 
+ HardWare: 
+ Outputs Active high/low
+ IN1 = PINC.0
+ IN2 = PINC.1
+ IN3 = PINC.2
+ IN4 = PINC.3
+ 
+ Inputs (pushbuttons used to select which mode) 
+ SW0 = PINA.0 WaveStep mode
+ SW1 = PINA.1 FullStep mode
+ SW2 = PINA.2 HalfStep mode
+ 
+ *////////////////////////////////////////////////////////////////////////
+  
+
+#define F_CPU 16000000UL
+#define PushButton PINA //used for checkoff 2&3 defines "PushButton" as PINA
+#include <avr/io.h>
+#include "Debugger.h" //includes Debugger.h 
+//#include "stepper_motor.h" //includes stepper_motor.h file
+
+//checkoff1
+/*
+void io_init(void) //initialize io ports
+{
+	//inputs
+	DDRA=(0x00); 
+	PORTA=(0xFF);
+	//outputs
+	DDRC = 0xFF;
+	PORTC = 0x00; 
+	
+}
+
+int main(void)
+{
+	io_init(); //call initialized io ports function
+    initDebug(); //call debug function 
+    PORTC=0x55; //PORTC follows 01010101 output to make LED pattern on driver board
+    while (1) 
+    { 
+    }
+	
+}
+*/
+
+//checkoff2
+
+void io_init(void); //initialize io ports
+
+int main(void)
+{
+	io_init(); //call initialized io ports function
+	while (1)
+	{
+		uint16_t n = 2;  // declares an unsigned 16 bit integer variable "n" to 2
+		switch (PushButton) //switch case for Wave, Full, and half step mode depending on which Pushbutton is pressed from PINA
+		{
+			case Wave_step:   //if SW0 is pressed, start Wave Step mode
+			{
+				Stepper_Drive('W',n); //call stepper_drive function for 2 revolutions in wave step mode
+				break; //break out of case
+			}
+			case Full_step:  //if SW1 is pressed, Full Step mode
+			{
+				Stepper_Drive('F',n); //call stepper_drive function for 2 revolutions in full step mode
+				break; //break out of case
+			}
+			case Half_step: //if SW1 is pressed, Half Step mode
+			{
+				Stepper_Drive('H',n); //call stepper_drive function for 2 revolutions in half step mode
+				break; //break out of case
+			}
+		}
+		stepper_output = 0x00;  //clear output when done
+	}
+}
+
+
+
+void io_init(void) //initialize io ports
+{
+	//inputs
+	DDRA=(0x00); //set all 8 pins on PORTA as i/p
+	PORTA=(0xFF); //All pull-ups set
+	//outputs
+	DDRC = 0xFF; //All PORTC set to o/p to drive Stepper Motor
+	PORTC = 0x00; //All output initially off 
+	
+}
+
+
+
+//checkoff 3
+/*
+int main(void)
+{
+	io_init();
+	while (1)
+	{
+		switch (PushButton)
+		{
+			case clockwise:   //if SW0 is pressed, start Wave Step mode clockwise
+			{
+				stepper_movement('W',2); //call stepper_movement function clockwise in wave step mode
+				break; //break out of case
+			}
+			case counterclockwise: //if SW1 is pressed, start Wave Step mode counterclockwise
+			{
+				stepper_movement('C',2); //call stepper_movement function counterclockwise in wave step mode
+				break; //break out of case
+			}
+			
+		}
+		stepper_output = 0x00;  //clear output when done
+	}
+}
+
+
+
+void io_init(void) //initialize io ports
+{
+	//inputs
+	DDRA=(0x00); //set all 8 pins on PORTA as i/p
+	PORTA=(0xFF); //All pull-ups set
+	//outputs
+	DDRC = 0xFF; //All PORTC set to o/p to drive Stepper Motor
+	PORTC = 0x00; //All output initially off
+	
+}
+*/
---
+++ Lab3/stepper_motor.c
@@ -0,0 +1,92 @@
+/*
+ * stepper_motor.c
+ *
+ * Created: 9/6/2023 3:44:50 PM
+ *  Author: Caiden Moreno
+ */ 
+#include "stepper_motor.h" //calls stepper_motor.h file
+//checkoff2
+
+uint8_t Wave[4] = {1,2,4,8}; //array for Wave step mode
+uint8_t Full[4] = {3,6,0x0C,9}; //array for full step mode
+uint8_t Half[8] = {9,1,3,2,6,4,0x0C,8};//array for half step mode
+	
+void Stepper_Drive(char mode, uint8_t revolutions) //stepper drive function 
+{
+	uint16_t steps = 512; //declares an unsigned 16 bit integer variable "steps" to 512
+	 
+	switch (mode)
+	{
+		case 'W': //wave step mode
+			steps=512*revolutions;  //calculate the steps needed for each revolution
+			for(uint16_t i=0; i<steps; i++) //loop through the array based on the calculated steps needed for each revolution
+			{
+				for(uint16_t j=0; j<4; j++)//loop through the whole array
+				{
+					stepper_output = Wave[j]; //use array
+					_delay_ms(3); //3 ms delay
+				}
+			}
+		break; //break from case
+		case 'F': //full step mode
+		steps=512*revolutions;//calculated steps needed for each revolution.
+		for(uint16_t i=0; i< steps; i++)//loop through the array based on the calculated steps needed for each revolution
+		{
+			for(uint16_t j=0; j<4; j++) //loop through the whole array
+			{
+				stepper_output = Full[j]; //use the array
+				_delay_ms(3); //3 ms delay
+			}
+		}
+	break; //break from case
+	case 'H': //half step mode
+	steps=512*revolutions; //calculated steps needed for each revolution.
+	for(uint16_t i=0; i<steps; i++) //loop through the array based on the calculated steps needed for each revolution
+	{
+		for(uint16_t j=0; j<8; j++)//loop through the whole array
+		{
+		stepper_output = Half[j]; //use the array
+			_delay_ms(3); //3 ms delay
+		}
+	}
+break; //break from case
+	}
+}
+
+
+//checkoff3
+/*
+uint8_t Wave[4] = {1,2,4,8}; //clockwise wavestep array 
+uint8_t Wavey[4]= {8,4,2,1};   //counterclockwise wavestep array 
+
+void stepper_movement(char mode, uint8_t revolutions) //stepper movement function 
+{
+	switch (mode)
+	{
+		uint16_t steps = 512; //declares an unsigned 16 bit integer variable "steps" to 512
+		case 'W': //clockwise wave step
+		steps=512*revolutions;  //calculate the steps needed for each revolution
+		for(uint16_t i=0; i<steps; i++) //loop through the array based on the calculated steps needed for each revolution
+		{
+			for(uint16_t j=0; j<4; j++)//loop through the whole array
+			{
+				stepper_output = Wave[j];  //use array
+				_delay_ms(3); //3ms delay
+			}
+		}
+		break;
+		
+		case 'C': //counterclockwise wave step
+		steps=512*revolutions;//calculated the steps needed for each revolution.
+		for(uint16_t i=0; i< steps; i++)//loop through the array based on the calculated steps needed for each revolution
+		{
+			for(uint16_t j=0; j<4; j++)//loop through the whole array
+			{
+				stepper_output = Wavey[j]; //use the array
+				_delay_ms(3); //3 ms delay
+			}
+		}
+		break;//break from case
+   }
+}
+*/
