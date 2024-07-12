7735aefa8753e856a10b621856d0cb7c2e9c9fff
Caiden701 <moreno.caiden02@gmail.com>
2024-07-12 15:19:59 -0400

    Add files via upload

---
+++ Lab4and5/main.c
@@ -0,0 +1,18 @@
+#include <avr/io.h> // Include the appropriate AVR library for your microcontroller
+
+int main() {
+	// Set OC1A (connected to PORTB.5) as an output
+	DDRB = (1 << DDB5);
+
+	// Set OC3A (connected to PORTE.5) as an output
+	DDRE = (1 << DDE5);
+
+	// Your code goes here
+
+	return 0;
+}
+
+
+
+
+
