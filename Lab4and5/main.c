96286118a73957d70e5e9c4737d18b19fbaf47c5
Caiden701 <moreno.caiden02@gmail.com>
2024-07-12 15:22:05 -0400

    Add files via upload

---
+++ Lab4and5/main.c
@@ -0,0 +1,18 @@
+/*
+ * control_stg_2.c
+ *
+ * Created: 10/1/2023 1:49:05 PM
+ * Author : moren
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
